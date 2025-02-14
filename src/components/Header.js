9413660cb24c09504273cf8a793fa69cec06b418
Sitong <nickren93@gmail.com>
2025-02-14 00:50:07 -0500

    complete lab

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header( {onDarkModeClick, mode}) {
+
+  return (
+    <div className={"App " + (mode ? "dark" : "light")}>
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {mode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
